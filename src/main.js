cf4fed52ddfbd6b5037320cd7f73ba462eeb2b94
yabai <rohjihyun95@gmail.com>
2024-01-02 01:27:02 +0900

    default app compl

--- src/main.js
+++ src/main.js
@@ -1,15 +1,58 @@
-const {app, BrowserWindow, components, ipcMain, ipcRenderer} = require('electron');
+const {app, BrowserWindow, components, ipcMain, ipcRenderer, Menu, MenuItem} = require('electron');
 // * `widevinecdm.dll` on Windows.
 // app.commandLine.appendSwitch('widevine-cdm-path', 'C:\\Program Files\\Google\\Chrome\\Application\\120.0.6099.130\\WidevineCdm\\_platform_specific\\win_x64\\widevinecdm.dll')
 // // The version of plugin can be got from `chrome://components` page in Chrome.
 // app.commandLine.appendSwitch('widevine-cdm-version', '1.0.2738.0')
 const path = require('node:path')
 const laftel = require('./scripts/laftel')
+let mainWindow =null
+function check_item(mitem, win, event){
+  if (mitem.label == "netflix")
+    mainWindow.loadURL('https://netflix.com/');
+  else if(mitem.label == "laftel"){
+    mainWindow.loadURL('https://laftel.net/');
+  }else if(mitem.label == "disney"){
+    mainWindow.loadURL('https://www.disneyplus.com/');
+  }
+}
+const menu_templete=[
+  {
+    label: 'OTT',
+    submenu:[
+      {
+        label : 'netflix',
+        accelerator: 'CommandOrControl+N',
+        click : check_item
+      },
+      {
+        label:'laftel',
+        accelerator: 'CommandOrControl+L',
+        click : check_item
+      },
+      {
+        label:'disney',
+        accelerator: 'CommandOrControl+D',
+        click : check_item
+      }
+    ]
+    
+  },
+  {
+    label : "help"
+  }
+  
+];
+
+
+
+let newMenu= Menu.buildFromTemplate(menu_templete);
+
+
+
+
 
 app.disableHardwareAcceleration();
-let mainWindow =null
 function createWindow () {
-
   mainWindow = new BrowserWindow(
 
     {
@@ -18,14 +61,19 @@ function createWindow () {
       // maxWidth : 800,
       // maxHeight : 600,
       plugins: true,
+      autoHideMenuBar : true,
+      icon: path.join(__dirname, '../resources/icons/kawaikara.ico'),
+
       webPreferences: {
         preload: path.join(__dirname, 'preload.js')
       }
+
 
   }
   );
   mainWindow.loadURL('https://laftel.net/');
 
+  mainWindow.setMenu(newMenu)
 
   mainWindow.on('fullscreenchange', () => {
     console.log("fullscreenchange")
@@ -34,8 +82,7 @@ function createWindow () {
 
 mainWindow.setFullScreenable(false)
 // mainWindow.setFullScreenable(false)
-mainWindow.setMaximizable(false)
-mainWindow.setMenu(null);
+// mainWindow.setMaximizable(false)
 mainWindow.on('resize', function () {
   var size   = mainWindow.getSize();
   var width  = size[0];
@@ -67,7 +114,6 @@ mainWindow.webContents.on('will-navigate', ()=>{
 };
 ipcMain.on("open-url", ()=>{console.log("open url")});
 ipcMain.on("fullscreen", ()=>{console.log("fullscreen")});
-
 
 
 
