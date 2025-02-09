60163e123dfd45d453b323f60d0a97a579965207
yabai <rohjihyun95@gmail.com>
2025-02-10 01:07:36 +0900

    add mac password secrest

--- scripts/make_p12_to-base64.sh
+++ scripts/make_p12_to-base64.sh
@@ -1,7 +1,7 @@
 mkdir ./private
 
-openssl genpkey -algorithm RSA -out ./private/private-key-mac.pem 
+openssl genpkey -algorithm RSA -out ./private/private-key-mac.pem -aes256
 openssl req -new -key ./private/private-key-mac.pem -out ./private/my-cert.csr
 openssl x509 -req -in ./private/my-cert.csr -signkey ./private/private-key-mac.pem -out ./private/my-cert.crt
-openssl pkcs12 -export -in ./private/my-cert.crt -inkey ./private/private-key-mac.pem -out ./private/my-cert.p12 -nodes
+openssl pkcs12 -export -in ./private/my-cert.crt -inkey ./private/private-key-mac.pem -out ./private/my-cert.p12 
 certutil -encode ./private/my-cert.p12 ./private/my-cert.b64
