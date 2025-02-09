4fc5a627075ea52c0c64cfc982411d7cb9faff92
yabai <rohjihyun95@gmail.com>
2025-02-10 00:54:15 +0900

    add mac reauth

---
+++ scripts/make_p12_to-base64.sh
@@ -0,0 +1,7 @@
+mkdir ./private
+
+openssl genpkey -algorithm RSA -out ./private/private-key-mac.pem 
+openssl req -new -key ./private/private-key-mac.pem -out ./private/my-cert.csr
+openssl x509 -req -in ./private/my-cert.csr -signkey ./private/private-key-mac.pem -out ./private/my-cert.crt
+openssl pkcs12 -export -in ./private/my-cert.crt -inkey ./private/private-key-mac.pem -out ./private/my-cert.p12 -nodes
+certutil -encode ./private/my-cert.p12 ./private/my-cert.b64
