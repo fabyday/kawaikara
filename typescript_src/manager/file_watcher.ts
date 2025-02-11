4e8099ed77eec9d3b1e5a1fa76c04152d9ac1b61
yabai <rohjihyun95@gmail.com>
2025-02-11 20:10:39 +0900

    add filewatcher

--- typescript_src/manager/file_watcher.ts
+++ typescript_src/manager/file_watcher.ts
@@ -1,21 +1,18 @@
+import { EventEmitter } from 'stream';
+
 class KawaiFileWatcher {
     static __instance: KawaiFileWatcher | null = null;
+
+    m_event_emitter: EventEmitter;
     static getInstance() {
         if (KawaiFileWatcher.__instance) {
             KawaiFileWatcher.__instance = new KawaiFileWatcher();
         }
-        return KawaiFileWatcher.__instance
+        return KawaiFileWatcher.__instance;
     }
 
-
-
-
-    private constructor(){
-        // 
+    private constructor() {
+        this.m_event_emitter = new EventEmitter();
+        this.m_event_emitter.setMaxListeners(50);
     }
-
-
-
-
-    
 }
