9fba0a4d26d3ab38f26f60edf80e927640908e09
fabyday <rohjihyun95@gmail.com>
2025-03-23 18:52:32 +0900

    add auth scripts

--- scripts/executable_auth_scripts.js
+++ scripts/executable_auth_scripts.js
@@ -1,12 +1,12 @@
 const { execSync } = require("child_process");
 const path = require("path");
 const os = require("os");
-
+const fs = require("node:fs")
 
 
 
 if (os.platform() === "darwin") { // Mac 전용
-    const binPath = path.join(__dirname, "..", "thirdparty", "darwin", "bin");
+    const binPath = path.join(__dirname, "..", "thirdparty", "bin", "darwin");
 
     try {
         if (fs.existsSync(binPath)) {
@@ -20,5 +20,5 @@ if (os.platform() === "darwin") { // Mac 전용
         process.exit(1);
     }
 } else {
-    console.log("ℹ️it's not darwin");
+    console.log("ℹ️ it's not darwin");
 }
