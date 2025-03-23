152e2b5148217ef4775e0172ca67b822c2e030e8
yabai <rohjihyun95@gmail.com>
2025-03-23 18:41:44 +0900

    add auth

---
+++ scripts/executable_auth_scripts.js
@@ -0,0 +1,24 @@
+const { execSync } = require("child_process");
+const path = require("path");
+const os = require("os");
+
+
+
+
+if (os.platform() === "darwin") { // Mac 전용
+    const binPath = path.join(__dirname, "..", "thirdparty", "darwin", "bin");
+
+    try {
+        if (fs.existsSync(binPath)) {
+            execSync(`chmod -R a+rx "${binPath}"`);
+            console.log(`✅ auth succ: ${binPath}`);
+        } else {
+            console.warn(`⚠️ directory Not Exists: ${binPath}`);
+        }
+    } catch (error) {
+        console.error("❌ auth failed:", error);
+        process.exit(1);
+    }
+} else {
+    console.log("ℹ️it's not darwin");
+}
