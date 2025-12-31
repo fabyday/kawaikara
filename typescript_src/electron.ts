d4299f65167e102ea9fa7a76877cd621f68b0b39
yabai <rohjihyun95@gmail.com>
2026-01-01 01:14:24 +0900

    modify comment on main function

--- typescript_src/electron.ts
+++ typescript_src/electron.ts
@@ -54,47 +54,6 @@ async function importCookiesToElectron(cookies: Cookie[]) {
 app.whenReady().then(async () => {
     log.info('electron-castlab : component initialize...');
     await components.whenReady();
-    // console.log('test0');
-    // const browser = await chromium.launchPersistentContext('./useData', {
-    //     headless: false,
-    //     channel: 'chrome',
-    //     viewport: null,
-    // });
-    // console.log('test1');
-
-    // const page = browser.pages()[0] || (await browser.newPage());
-    // console.log('test3');
-
-    // await page.goto('https://www.coupangplay.com', {});
-    // try {
-    //     // 3. /home이 포함된 URL로 바뀔 때까지 최대 5초 대기
-    //     await page.waitForURL(/\/(home|profile)/, { timeout: 5000 });
-    //     console.log('login succ');
-    // } catch (error) {
-    //     console.log('loging err');
-    //     // 현재 URL 확인해보기
-    //     console.log('현재 위치:', page.url());
-    // }
-
-    // // await page.setViewportSize({ width: 1080, height: 1024 });
-    // await page.once('close', async () => {
-    //     console.log('test');
-    //     const cookies = await page.context().cookies();
-
-    //     console.log('test32');
-    //     // test(cookies);
-    //     await importCookiesToElectron(cookies);
-    //     const view = new BrowserWindow({
-    //         webPreferences: {},
-    //     });
-    //     console.log('test323');
-    //     view.loadURL('https://www.coupangplay.com', {
-    //         userAgent:
-    //             'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
-    //     });
-    //     console.log('test32322');
-    // });
-
     log.info('components ready:', components.status());
     log.debug('initialize kawaikara components...');
     await initialize();
